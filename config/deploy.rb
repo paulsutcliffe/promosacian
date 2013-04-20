8c418da3542f8a4185b18fe678a5a767b9d1e767
Paul Sutcliffe <paul@kosmyka.com>
2013-04-20 17:10:22 -0500

    initial commit

---
+++ config/deploy.rb
@@ -0,0 +1,58 @@
+require "bundler/capistrano"
+require "rvm/capistrano"
+
+set :rvm_ruby_string, '1.9.3'
+set :rvm_type, :user  # Don't use system-wide RVM
+
+server "sacian.com", :web, :app, :db, primary: true
+
+set :application, "promosacian"
+set :user, "paul"
+set :deploy_to, "/var/www/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, "git"
+set :repository, "git@github.com:paulsutcliffe/#{application}.git"
+set :branch, "master"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
+namespace :deploy do
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
+    end
+  end
+
+  task :setup_config, roles: :app do
+    if File.exist? '/etc/nginx/sites-enabled/default'
+      sudo "rm /etc/nginx/sites-enabled/default"
+    end
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    sudo "mkdir -p #{shared_path}/config"
+    sudo "wget --no-check-certificate 'https://raw.github.com/paulsutcliffe/stereosonica/master/config/database.example.yml' -O #{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Promosacian::Application.config.secret_token = '8d351b068f988def6218e857e8d50e0a029c4eb0667eaa9b44aa19ed29d8a649a41378aacdca64540cbddb118b921cd6783f8bfd62b4f771320cdf5c1d9f29ff'
