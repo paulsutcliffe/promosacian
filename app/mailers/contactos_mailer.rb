e505fa8838dab7bdc2842a2ba0f29f798e420ec5
Paul Sutcliffe <paul@kosmyka.com>
2013-05-03 21:34:43 -0500

    Implemented Mailers

--- app/mailers/contactos_mailer.rb
+++ app/mailers/contactos_mailer.rb
@@ -7,11 +7,6 @@ class ContactosMailer < ActionMailer::Base
     mail(:to => "#{contact.nombre} <#{contact.email}>", :reply_to => contact.email, :subject => "SACIAN")
   end
 
-  def contact_registration_uy(contact)
-    @contacto = contact
-    mail(:to => "lainuru@montevideo.com.uy", :subject => "Mensaje desde la web de SACIAN Uruguay")
-  end
-
   def contact_registration_bo(contact)
     @contacto = contact
     mail(:to => "lain1004scz@entelnet.bo", :subject => "Mensaje desde la web de SACIAN Bolivia")
@@ -22,19 +17,9 @@ class ContactosMailer < ActionMailer::Base
     mail(:to => "laincr@racsa.co.cr", :subject => "Mensaje desde la web de SACIAN Costa Rica")
   end
 
-  def contact_registration_gt(contact)
-    @contacto = contact
-    mail(:to => "lessguatemala@terra.com.gt", :subject => "Mensaje desde la web de SACIAN Guatemala")
-  end
-
   def contact_registration_pe(contact)
     @contacto = contact
     mail(:to => "contacto@lainadelgaza.net", :subject => "Mensaje desde la web de SACIAN Perú")
-  end
-
-  def contact_registration_sv(contact)
-    @contacto = contact
-    mail(:to => "lainelsalvador@amnetsal.com", :subject => "Mensaje desde la web de SACIAN El Salvador")
   end
 
 end
